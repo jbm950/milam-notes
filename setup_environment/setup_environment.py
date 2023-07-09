2972a8635f03f9635e2c027217dcf1de5e5ca29b
jbm950 <jmilam343@gmail.com>
2023-07-09 16:57:23 -0400

    feat(Setup Environment): add basic script to create environment variables
    This repository makes use of environment variables in its configuration
    files (ex. vimrc, bashrc). This commit adds a python script that will go
    through and ask a user to provide definition for any of these variables
    that do not already exist on a computer so that they're available for
    the configuration files.
    
    Refs:

---
+++ setup_environment/setup_environment.py
@@ -0,0 +1,34 @@
+import os
+import pathlib
+import subprocess
+
+
+def set_env_variable(env_variable, env_variable_name):
+    """This function will set environment variables for windows if not already
+    set. Note that this sets directories to use unix style path separators so
+    the environment variables are only good in programs that recognize that
+    format.
+
+    Parameters
+    ----------
+    env_variable : str
+        This is what the environment variable will go by in the code.
+    env_variable_name : str
+        This is the human readable name for the environment variable.
+    """
+
+    if os.getenv(env_variable) is None:
+        env_variable_path = pathlib.Path(input(f"What is the path to the {env_variable_name} on "
+                                               "this computer?:  "))
+        if env_variable_path.is_dir():
+            env_variable_path = str(env_variable_path).replace("\\", "/")
+            subprocess.Popen(f'setx {env_variable} "{env_variable_path}"')
+
+
+if __name__ == "__main__":
+    env_variables = [("NOTES_DIR", "notes directory"),
+                     ("MILAM_NOTES_REPO_DIR", "milam notes repository"),
+                     ("GIT_REPOS", "git repositories"),
+                     ("CUSTOM_BATCH_SCRIPTS_PATH", "custom batch scripts")]
+    for env_variable in env_variables:
+        set_env_variable(*env_variable)
