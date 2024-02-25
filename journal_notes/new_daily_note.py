20b793d895f24165c2605084c469a9c83d123845
jbm950 <jmilam343@gmail.com>
2024-02-24 19:45:07 -0500

    refactor(Journal Notes): extract previous daily note function
    This commit extracts the code that finds a previous daily note to a new
    function. This cleans up the "main" block to focus on high level
    procedures and not get bogged down in the details of how they work.
    
    Refs:

--- journal_notes/new_daily_note.py
+++ journal_notes/new_daily_note.py
@@ -3,26 +3,16 @@ import pathlib
 import sys
 
 
-def new_daily_note(arg_notes_dir):
+
+def new_daily_note(notes_dir):
     today = datetime.date.today()
-    previous_date = datetime.date.today()
+    current_day_note_exists_already = notes_dir.joinpath(today.isoformat() + ".md").is_file()
+    if current_day_note_exists_already:
+        return
 
     # Assuming anything more than 50 days old doesn't need to be copied into
     # current note.
-    for i in range(50):
-        test_file_name = previous_date.isoformat() + ".md"
-        test_file_path = arg_notes_dir.joinpath(test_file_name)
-
-        current_day_note_already_exists = test_file_path.is_file() and i == 0
-        if current_day_note_already_exists:
-            return
-        elif test_file_path.is_file():
-            previous_daily_note_path = test_file_path
-            break
-
-        previous_date = previous_date - datetime.timedelta(days=1)
-    else:
-        previous_daily_note_path = None
+    previous_daily_note_path = find_prev_daily_note(day_range=50, notes_dir=notes_dir)
 
     if previous_daily_note_path is not None:
         projects_section_found = False
@@ -56,8 +46,44 @@ def new_daily_note(arg_notes_dir):
         new_daily_note.write(template)
 
 
+def find_prev_daily_note(day_range, notes_dir):
+    """This function will find the previous daily note in a given folder
+    assuming it follows isoformat. If the note is further than `day_range` days
+    ago, or if no note is found, the function will return None instead.
+
+    Parameters
+    ----------
+    day_range : int
+        This is the number of days considered "relevant" for the new daily
+        note. If a note is outside of this range, it will not be returned.
+    notes_dir : pathlib.Path
+        This is a path to the folder to search for the previous daily note.
+
+    Returns
+    -------
+    previous_daily_note_path : pathlib.Path or None
+        This is a path to the last daily note file. If no file was found for
+        the given `day_range`, None will be returned instead.
+    """
+
+    previous_date = datetime.date.today()
+    for i in range(1, day_range):
+        test_file_name = previous_date.isoformat() + ".md"
+        test_file_path = notes_dir.joinpath(test_file_name)
+
+        if test_file_path.is_file():
+            previous_daily_note_path = test_file_path
+            break
+
+        previous_date = previous_date - datetime.timedelta(days=1)
+    else:
+        previous_daily_note_path = None
+
+    return previous_daily_note_path
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise IndexError("Need to pass in a path to the folder to manage daily notes")
     notes_dir = pathlib.Path(sys.argv[1])
-    new_daily_note(arg_notes_dir=notes_dir)
+    new_daily_note(notes_dir=notes_dir)
